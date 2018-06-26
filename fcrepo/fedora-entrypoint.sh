6f1abf54a38267db049f3efc166ca91e8c9d012b
Michael Klein <mbklein@gmail.com>
2018-06-26 14:12:44 -0500

    Fix Fedora memory detection

--- fcrepo/fedora-entrypoint.sh
+++ fcrepo/fedora-entrypoint.sh
@@ -4,7 +4,18 @@ echo "Changing ownership of /data as $(whoami)"
 chown jetty:jetty /data
 echo "Downgrading privileges and resuming"
 
-MEM_BYTES=$(cat /sys/fs/cgroup/memory/memory.memsw.limit_in_bytes)
+for MEM_FILE in memory.limit_in_bytes memory.memsw.limit_in_bytes memory.kmem.limit_in_bytes; do
+  if [[ -e /sys/fs/cgroup/memory/${MEM_FILE} ]]; then
+    break
+  fi
+done
+
+if [[ ! -e /sys/fs/cgroup/memory/${MEM_FILE} ]]; then
+  echo "Could not read container memory. Exiting."
+  exit 1
+fi
+
+MEM_BYTES=$(cat /sys/fs/cgroup/memory/${MEM_FILE})
 if [[ $MEM_BYTES -ne "9223372036854771712" ]]; then
   let "MX=$MEM_BYTES * 8 / 10 / 1024 / 1024"
   echo "Setting -Xmx${MX}m"

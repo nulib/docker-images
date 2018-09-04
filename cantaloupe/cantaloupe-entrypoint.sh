057feb533408ef27746231f10814b63c00d857e3
Michael B. Klein <mbklein@gmail.com>
2018-09-04 11:15:02 -0500

    Update cantaloupe to allow specific S3 endpoint

--- cantaloupe/cantaloupe-entrypoint.sh
+++ cantaloupe/cantaloupe-entrypoint.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
 config_file="cantaloupe.properties"
-if [ "${TIFF_BUCKET}" != "" ]; then
+if [ -n "${TIFF_BUCKET}" ]; then
   echo "extends cantaloupe_s3_defaults.properties" > /etc/cantaloupe/cantaloupe_s3.properties
   echo "S3Source.BasicLookupStrategy.bucket.name = ${TIFF_BUCKET}" >> /etc/cantaloupe/cantaloupe_s3.properties
 
-  if [ "${AWS_REGION}" != "" ]; then
-    echo "S3Source.endpoint = s3.dualstack.${AWS_REGION}.amazonaws.com" >> /etc/cantaloupe/cantaloupe_s3.properties
+  if [ -z "${S3_ENDPOINT}" && -n "${AWS_REGION}" ]; then
+    S3_ENDPOINT="s3.dualstack.${AWS_REGION}.amazonaws.com"
   fi
-  if [ "${AWS_ACCESS_KEY_ID}" != "" ]; then
+
+  if [ -n "${S3_ENDPOINT}" ]; then
+    echo "S3Source.endpoint = ${S3_ENDPOINT}" >> /etc/cantaloupe/cantaloupe_s3.properties
+  fi
+
+  if [ -n "${AWS_ACCESS_KEY_ID}" ]; then
     echo "S3Source.access_key_id = ${AWS_ACCESS_KEY_ID}" >> /etc/cantaloupe/cantaloupe_s3.properties
     echo "S3Source.secret_key = ${AWS_SECRET_KEY}" >> /etc/cantaloupe/cantaloupe_s3.properties
   fi

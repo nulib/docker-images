44c399e99aafd60a6a4b9abf0a18044913f52993
Michael B. Klein <mbklein@gmail.com>
2018-09-21 14:11:28 -0500

    Fix syntax error in cantaloupe entrypoint script

--- cantaloupe/cantaloupe-entrypoint.sh
+++ cantaloupe/cantaloupe-entrypoint.sh
@@ -5,7 +5,7 @@ if [ -n "${TIFF_BUCKET}" ]; then
   echo "extends cantaloupe_s3_defaults.properties" > /etc/cantaloupe/cantaloupe_s3.properties
   echo "S3Source.BasicLookupStrategy.bucket.name = ${TIFF_BUCKET}" >> /etc/cantaloupe/cantaloupe_s3.properties
 
-  if [ -z "${S3_ENDPOINT}" && -n "${AWS_REGION}" ]; then
+  if [ -z "${S3_ENDPOINT}" ] && [ -n "${AWS_REGION}" ]; then
     S3_ENDPOINT="s3.dualstack.${AWS_REGION}.amazonaws.com"
   fi
 

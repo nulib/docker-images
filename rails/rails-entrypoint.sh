63b2fc4a0b0646fd5c09f2c230227621b5b7fce4
Michael B. Klein <mbklein@gmail.com>
2021-11-12 11:44:53 -0600

    Remove obsolete images

--- rails/rails-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-chown app:app /home/app/gems
-chown -R app:app /home/app/code/tmp /home/app/code/public
-sudo -u app bash <<__EOF__
-bundle install --path /home/app/gems
-bundle exec rails server
-__EOF__

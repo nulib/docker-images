2aeb5587e63536d329974ee7e6a6b93702d8d682
Michael B. Klein <mbklein@gmail.com>
2024-01-12 15:17:29 -0600

    Add support to create the seeded dev environment LDAP image

---
+++ ldap-alpine/seeds/seed.sh
@@ -0,0 +1,12 @@
+#!/bin/ash
+
+set -e
+
+sed -i s/nodaemon=true/nodaemon=false/ /etc/supervisord.conf
+/usr/bin/supervisord
+while ! nc -z -w 5 localhost 389; do sleep 1; done
+for f in $(dirname $0)/*.ldif; do 
+  ldapmodify -H "ldap://localhost:389" -D "cn=Administrator,cn=Users,dc=library,dc=northwestern,dc=edu" -w "d0ck3rAdm1n!" -c -f $f;
+done
+killall supervisord
+sed -i s/nodaemon=false/nodaemon=true/ /etc/supervisord.conf

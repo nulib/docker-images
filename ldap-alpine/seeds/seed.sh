cbbea07d24c4b1fd4f7a19942cbca00f1b36b638
Michael B. Klein <mbklein@gmail.com>
2025-04-28 12:30:00 -0500

    Update LDAP seeds

--- ldap-alpine/seeds/seed.sh
+++ ldap-alpine/seeds/seed.sh
@@ -8,5 +8,5 @@ while ! nc -z -w 5 localhost 389; do sleep 1; done
 for f in $(dirname $0)/*.ldif; do 
   ldapmodify -H "ldap://localhost:389" -D "cn=Administrator,cn=Users,dc=library,dc=northwestern,dc=edu" -w "d0ck3rAdm1n!" -c -f $f;
 done
-killall supervisord
+killall supervisord || true
 sed -i s/nodaemon=false/nodaemon=true/ /etc/supervisord.conf
